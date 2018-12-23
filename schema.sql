3e59d6020e6a8c3c15653a1ef58fe3cdb0b37fd0
Nate <nathanw88@gmail.com>
2018-12-23 12:38:03 -0600

    some minor changes getting heroku up

--- schema.sql
+++ schema.sql
@@ -1,7 +1,11 @@
 CREATE DATABASE profile_db;
 
+USE profile_db;
+
 CREATE TABLE IF NOT EXISTS profiles (
+   id int(55)AUTO_INCREMENT NOT NULL,
    name VARCHAR(25),
    photo VARCHAR(255),
-   scores VARCHAR(25)
+   scores VARCHAR(25),
+   PRIMARY KEY (id)
 );
---
+++ seeds.sql
@@ -0,0 +1,7 @@
+
+
+INSERT INTO profiles (name, photo, scores) VALUES
+("Eeyore", "./public/survey_files/images/Eeyore.jpg", "0,0,0,0,0,0,0,0,0,0" ),
+("Postive Pete", "./survey_files/images/positive.png", "5,5,5,5,5,5,5,5,5,5"),
+("Sam", "./survey_files/images/sam.jpg", "5,3,2,1,4,4,2,3,3,2"),
+("Paul", "./survey_files/images/paul.jpg", "2,2,1,5,2,1,3,4,4,6" );
